35f59f1c66e2d51d722de15426b10933eb744cd6
Andriy N <andriy.nikolaenko1012@gmail.com>
2016-09-29 23:43:02 +0300

    working with privacyterm screen

--- app/src/main/java/com/example/developer/comivo/activities/activities_for_settings/PrivacyTermActivity.java
+++ app/src/main/java/com/example/developer/comivo/activities/activities_for_settings/PrivacyTermActivity.java
@@ -3,23 +3,39 @@ package com.example.developer.comivo.activities.activities_for_settings;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.developer.comivo.R;
+import com.example.developer.comivo.network.ServerResponseParsing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class PrivacyTermActivity extends AppCompatActivity {
+
+    public WebView privacyTerm;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +59,8 @@ public class PrivacyTermActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        new PrivacyTerm().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,5 +69,92 @@ public class PrivacyTermActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private class PrivacyTerm extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String data = getJSON("http://beta.comivo.com/mobileapi/cms/cmstype?type=Privacy_Policy", 5000);
+
+
+            return data;
+        }
+
+
+        @Override
+        protected void onPostExecute(String responseString) {
+            //super.onPostExecute(s);
+
+            if (PrivacyTermActivity.this.isFinishing()) {
+                return;
+            }
+
+            ServerResponseParsing serverResponseParsing = ServerResponseParsing.getInstance();
+            serverResponseParsing.parseMembershipAndPrivacy(responseString);
+
+            Log.d("test LOG", "Parsed string: status " + serverResponseParsing.getStatus() +
+                    " cmsType " + serverResponseParsing.getCmsPageTypeId() +
+                    " title" + serverResponseParsing.getTitle() +
+                    " content" + serverResponseParsing.getContent() +
+                    " isActive" + serverResponseParsing.getIsActive() +
+                    " type" + serverResponseParsing.getType() +
+                    " message" + serverResponseParsing.getMessage() +
+                    " \n = " + responseString
+            );
+
+            String data = serverResponseParsing.getContent();
+            privacyTerm = (WebView) findViewById(R.id.wv_privacy_term);
+            privacyTerm.getSettings().setJavaScriptEnabled(true);
+
+            privacyTerm.loadDataWithBaseURL("", data, "text/html", "UTF-8", "");
+        }
+
+        public String getJSON(String url, int timeout) {
+            HttpURLConnection c = null;
+            try {
+                URL u = new URL(url);
+                c = (HttpURLConnection) u.openConnection();
+                c.setRequestMethod("GET");
+                c.setRequestProperty("Content-length", "0");
+                c.setRequestProperty("platform", "android");
+                c.setRequestProperty("version", "1.0.0");
+                c.setUseCaches(false);
+                c.setAllowUserInteraction(false);
+                c.setConnectTimeout(timeout);
+                c.setReadTimeout(timeout);
+                c.connect();
+                int status = c.getResponseCode();
+
+                switch (status) {
+                    case 200:
+                    case 201:
+                        BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line+"\n");
+                        }
+                        br.close();
+                        return sb.toString();
+                }
+
+
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (c != null) {
+                    try {
+                        c.disconnect();
+                    } catch (Exception ex) {
+                        Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
